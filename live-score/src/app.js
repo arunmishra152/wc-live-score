99c46dd32c5f082484c56e9c2a1e0c591f65d873
rana <arunmishra152@gmail.com>
2019-07-03 15:59:26 +0530

    change port

--- live-score/src/app.js
+++ live-score/src/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3004
+const port = process.env.PORT || 3000
 const axios = require('axios');
 
 app.get('/',async (req,res)=>{
